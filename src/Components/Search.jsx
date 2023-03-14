b663e5f2193edc995816922f515d85720be24767
itsShubhamShinde <itsshubhamks@gmai.com>
2023-03-14 18:54:57 +0530

    update

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -4,7 +4,7 @@ import './Search.css'
 const Search = ({ inputvalue, setInputvalue, value, setValue }) => {
 
     return (
-        <div className='Search' >
+        <div className='Search' style={{position:"fixed",top:"0" , left:"0"}}>
             <input type="text" onChange={(e) => {
                 setValue(e.currentTarget.value)
             }} />
